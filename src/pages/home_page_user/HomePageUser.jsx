b6f8acf33f031514de0d67c7f9d1de64d1d9e884
Joamcriff <tan.nguyen3@monstar-lab.com>
2023-12-01 21:50:20 +0700

    user

--- src/pages/home_page_user/HomePageUser.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-
-const HomePageUser = () => {
-    return(
-        <div>HomepageUser</div>
-    )
-}
-export default HomePageUser
--- src/pages/home_screen/HomeScreen.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-
-const HomeScreen = () => {
-    return(
-        <div>hello qwe</div>
-    )
-}
-export default HomeScreen
